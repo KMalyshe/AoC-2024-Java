01aa2d3660ce5c86c1a85f1698dbfa90257ca524
KMalyshe <kyryl.malyshev@gmail.com>
2024-12-03 21:02:31 +0100

    we love regex in this household

--- src/Day3.java
+++ src/Day3.java
@@ -12,20 +12,43 @@ public class Day3 {
             fullText += line.replace("\n", "");
         }
 
-        List<String> matches = new ArrayList<String>();
-        Matcher match = Pattern.compile("mul\\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\\)").matcher(fullText);
-        while (match.find()) {
-            matches.add(match.group());
-        }
+        List<String> matchesPart1 = regex(fullText, "mul\\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\\)");
+        List<String> matchesPart2 = regex(fullText, "(?:mul\\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\\)|do\\(\\)|don't\\(\\))");
 
-        int count = 0;
+        int count1 = 0;
 
-        for (int i = 0; i<matches.size(); i++) {
-            String[] split = matches.get(i).split(",");
+        for (int i = 0; i<matchesPart1.size(); i++) {
+            String[] split = matchesPart1.get(i).split(",");
             split[0] = split[0].substring(4); split[1] = split[1].substring(0, split[1].length()-1);
-            count += Integer.valueOf(split[0]) * Integer.valueOf(split[1]);
+            count1 += Integer.valueOf(split[0]) * Integer.valueOf(split[1]);
         }
 
-        System.out.println(count);
+        System.out.println("Part 1: " + count1);
+
+        boolean doMult = true;
+        int count2 = 0;
+
+        for (int i = 0; i<matchesPart2.size(); i++) {
+            String match = matchesPart2.get(i);
+            if (match.equals("do()")) {doMult = true; continue;}
+            else if (match.equals("don't()")) {doMult = false; continue;}
+            else if (doMult) {
+                String[] split = match.split(",");
+                split[0] = split[0].substring(4); split[1] = split[1].substring(0, split[1].length()-1);
+                count2 += Integer.valueOf(split[0]) * Integer.valueOf(split[1]);
+            }
+        }
+
+        System.out.println("Part 2: " + count2);
+
+    }
+
+    private static List<String> regex(String text, String reg) {
+        List<String> list = new ArrayList<String>();
+        Matcher matchPart1 = Pattern.compile(reg).matcher(text);
+        while (matchPart1.find()) {
+            list.add(matchPart1.group());
+        }
+        return list;
     }
 }
