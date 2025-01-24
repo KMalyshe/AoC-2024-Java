c5985e6d2356f6638c79c17d96640c6e68b4710a
Kyryl Malyshev <malyskyr@students.zhaw.ch>
2025-01-24 21:59:46 +0100

    part 2

--- src/Day4.java
+++ src/Day4.java
@@ -14,11 +14,12 @@ public class Day4 {
             }
         }
 
-        int count = 0;
+        int count1 = 0;
+        int count2 = 0;
         for (int i = 0; i<charMatrix.size(); i++) {
             for (int j = 0; j<charMatrix.get(0).size(); j++) {
                 if (charMatrix.get(i).get(j) == 'X') {
-                    ArrayList<Offset> found = checkM(new int[]{i, j}, charMatrix);
+                    ArrayList<Offset> found = checkCharAdjacent('M',new int[]{i, j}, charMatrix);
                     for (Offset offset : found) {
                         // Letter at Offset = M -> Further letters must be A and S
 
@@ -27,22 +28,41 @@ public class Day4 {
                         // :(
                         try {
                             if (charMatrix.get(i + offset.y*2).get(j + offset.x*2) == 'A' 
-                            && charMatrix.get(i + offset.y*3).get(j + offset.x*3) == 'S') count++;
+                            && charMatrix.get(i + offset.y*3).get(j + offset.x*3) == 'S') count1++;
                         } catch (Exception e) {
                         }
+                    }
+                }
+                if (charMatrix.get(i).get(j) == 'A') {
+                    int countMas = 0;
+                    ArrayList<Offset> found = checkCharAdjacent('M',new int[]{i, j}, charMatrix);
+                    if (found.size() < 2) continue;
+                    for (Offset offset : found) {
+                        if (offset.x != 0 && offset.y != 0) {
+                            try {
+                                if (charMatrix.get(i - offset.y).get(j - offset.x) == 'S') countMas++;
+                            } catch (Exception e) {
+                            }
+                        }
+                    }
+                    if (countMas == 2) {
+                        System.out.println("Found X-MAS at " + i + ", " + j);
+                        count2++;
                     }
                 }
             }
         }
 
-        System.out.println("XMAS amount: " + count);
+        System.out.println("XMAS amount: " + count1);
+        System.out.println("X-MAS amount: " + count2);
 
     }
 
     //coords: [row, column]
-    public static ArrayList<Offset> checkM (int[] coordinates, ArrayList<ArrayList<Character>> charMatrix) {
-        ArrayList<Offset> foundM = new ArrayList<>();
-        //topmost row
+    
+    public static ArrayList<Offset> checkCharAdjacent (char search, int[] coordinates, ArrayList<ArrayList<Character>> charMatrix) {
+        ArrayList<Offset> foundChars = new ArrayList<>();
+        
         Offset[] offsets = new Offset[]{
             new Offset(0, -1),
             new Offset(-1, -1),
@@ -53,21 +73,21 @@ public class Day4 {
             new Offset(1, 0),
             new Offset(1, -1),
         };
-
+        
         for (Offset offset : offsets) {
             if (offset.y == -1 && coordinates[0] == 0) continue;
             if (offset.y == 1 && coordinates[0] == charMatrix.get(0).size()-1) continue;
             if (offset.x == -1 && coordinates[1] == 0) continue;
             if (offset.x == 1 && coordinates[1] == charMatrix.size()-1) continue;
 
-            if (charMatrix.get(coordinates[0] + offset.y).get(coordinates[1] + offset.x) == 'M') {
-                foundM.add(offset);
+            if (charMatrix.get(coordinates[0] + offset.y).get(coordinates[1] + offset.x) == search) {
+                foundChars.add(offset);
             }
         }
-      
-
-        return foundM;
+        return foundChars;
     }
+
+    
 
 }
 
